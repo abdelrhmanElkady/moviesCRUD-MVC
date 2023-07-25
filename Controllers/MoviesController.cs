a8dd7cc03d26eaee33cb57181fc3e17a17050f5f
abdelrahman <abdoelkadyabdoelkady@gmail.com>
2023-07-25 08:12:25 +0300

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using moviesCRUD.Models;
+using moviesCRUD.ViewModels;
+
+namespace moviesCRUD.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var movies = await _context.Movies.OrderByDescending(m=>m.Rate).ToListAsync();
+            return View(movies);
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = await _context.Genres.OrderBy(e=>e.name).ToListAsync()
+            };
+            
+            return View("MovieForm",viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(MovieFormViewModel model)
+        {
+            var files = Request.Form.Files;
+            var poster = files.FirstOrDefault();
+
+            using var dataStream = new MemoryStream();
+            await poster.CopyToAsync(dataStream);
+            var movies = new Movie
+            {
+                Title = model.Title,
+                GenreId = model.GenreId,
+                Year = model.Year, 
+                Rate = model.Rate,
+                Storyline = model.Storyline,
+                Poster = dataStream.ToArray()
+            };
+
+            _context.Movies.Add(movies);
+            _context.SaveChanges();
+           
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+                return NotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                GenreId = movie.GenreId,
+                Rate = movie.Rate,
+                Year = movie.Year,
+                Storyline = movie.Storyline,
+                Poster = movie.Poster,
+                Genres = await _context.Genres.OrderBy(m => m.name).ToListAsync()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(MovieFormViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            var movie = await _context.Movies.FindAsync(model.Id);
+
+            if (movie == null)
+                return NotFound();
+
+            var files = Request.Form.Files;
+
+            if (files.Any())
+            {
+                var poster = files.FirstOrDefault();
+
+                using var dataStream = new MemoryStream();
+
+                await poster.CopyToAsync(dataStream);
+
+                model.Poster = dataStream.ToArray();
+                movie.Poster = model.Poster;
+            }
+
+            movie.Title = model.Title;
+            movie.GenreId = model.GenreId;
+            movie.Year = model.Year;
+            movie.Rate = model.Rate;
+            movie.Storyline = model.Storyline;
+
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var movie = await _context.Movies.Include(m => m.Genre).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return View(movie);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+                return NotFound();
+
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+
+}
---
+++ ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,29 @@
+﻿using moviesCRUD.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace moviesCRUD.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(250)]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        [Range(1,10)]
+        public double Rate { get; set; }
+
+        [Required, StringLength(2500)]
+        public string Storyline { get; set; }
+
+        
+        public byte[] Poster { get; set; }
+
+        [Display(Name ="Genre")]
+        public byte GenreId { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
